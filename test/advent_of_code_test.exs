0a236227c1bd0d3df353bb12a85f72fc1cf7d39f
Jacob Lindgren <jlindgre@redhat.com>
2022-11-01 09:01:00 -0500

    rename test files for warmup

--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -11,31 +11,43 @@ defmodule AdventOfCodeTest do
     end
   end
 
-  test "dec01" do
+  test "2021dec01" do
     aocautotest(
-      AdventOfCode.read_file("dec01.txt"),
-      [{&AdventOfCode.TwentyTwentyOne.Dec01.first/1, 1715}, {&AdventOfCode.TwentyTwentyOne.Dec01.second/1, 1739}]
+      AdventOfCode.read_file("2021dec01.txt"),
+      [
+        {&AdventOfCode.TwentyTwentyOne.Dec01.first/1, 1715},
+        {&AdventOfCode.TwentyTwentyOne.Dec01.second/1, 1739}
+      ]
     )
   end
 
-  test "dec02" do
+  test "2021dec02" do
     aocautotest(
-      AdventOfCode.read_file("dec02.txt"),
-      [{&AdventOfCode.TwentyTwentyOne.Dec02.first/1, 1_855_814}, {&AdventOfCode.TwentyTwentyOne.Dec02.second/1, 1_845_455_714}]
+      AdventOfCode.read_file("2021dec02.txt"),
+      [
+        {&AdventOfCode.TwentyTwentyOne.Dec02.first/1, 1_855_814},
+        {&AdventOfCode.TwentyTwentyOne.Dec02.second/1, 1_845_455_714}
+      ]
     )
   end
 
-  test "dec03" do
+  test "2021dec03" do
     aocautotest(
-      AdventOfCode.read_file("dec03.txt"),
-      [{&AdventOfCode.TwentyTwentyOne.Dec03.first/1, 3_813_416}, {&AdventOfCode.TwentyTwentyOne.Dec03.second/1, 2_990_784}]
+      AdventOfCode.read_file("2021dec03.txt"),
+      [
+        {&AdventOfCode.TwentyTwentyOne.Dec03.first/1, 3_813_416},
+        {&AdventOfCode.TwentyTwentyOne.Dec03.second/1, 2_990_784}
+      ]
     )
   end
 
-  test "dec05" do
+  test "2021dec05" do
     aocautotest(
-      AdventOfCode.read_file("dec05.txt"),
-      [{&AdventOfCode.TwentyTwentyOne.Dec05.first/1, 4873}, {&AdventOfCode.TwentyTwentyOne.Dec05.second/1, 19472}]
+      AdventOfCode.read_file("2021dec05.txt"),
+      [
+        {&AdventOfCode.TwentyTwentyOne.Dec05.first/1, 4873},
+        {&AdventOfCode.TwentyTwentyOne.Dec05.second/1, 19472}
+      ]
     )
   end
 end
